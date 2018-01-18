22296e829c1978e7cb8173cd1fcae438d367730a
Andreas Böhrnsen <andreas@frontrunner.io>
2018-01-18 13:09:07 +0100

    upgrade ember

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,10 +1,7 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
+import $ from 'jquery';
 
-const {
-  $
-} = Ember
-
-export default Ember.Route.extend({
+export default Route.extend({
   model() {
     return $.ajax('/users');
   }
